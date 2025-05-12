a3b574747743062985387e02e9f9a378b192f379
iandol <iandol@seneca.org>
2025-05-12 10:08:56 +0800

    feat: :sparkles: Enhance ZPoller class by adding socket property and adding unregister method

--- +jzmq/ZPoller.m
+++ +jzmq/ZPoller.m
@@ -1,6 +1,7 @@
 classdef ZPoller < handle
     properties ( GetAccess = public, SetAccess = private )
         pointer
+        socket
     end
 
     methods
@@ -17,16 +18,27 @@ classdef ZPoller < handle
             arguments
                 obj
                 socket (1, 1)
-                events (1, 1) jzmq.PollEvent
+                events (1, 1)
             end
-
             obj.pointer.register(socket, events);
+            obj.socket = socket;
         end
+
+        function unregister(obj, socket)
+            arguments
+                obj
+                socket (1, 1)
+            end
+            if isempty(socket); socket = obj.socket; end
+            obj.pointer.unregister(socket);
+            obj.socket = [];
+        end
+
 
         function events = poll(obj, timeout)
             arguments (Input)
-                obj (1, 1) ZPoller
-                timeout (1, 1) double
+                obj (1, 1) jzmq.ZPoller
+                timeout (1, 1) double = 0
             end
             arguments (Output)
                 events (1, 1) double
@@ -38,33 +50,36 @@ classdef ZPoller < handle
         function result = isReadable(obj, socket)
             arguments (Input)
                 obj (1, 1) jzmq.ZPoller
-                socket (1, 1)
+                socket = []
             end
             arguments (Output)
-                result (1, 1) boolean
+                result (1, 1) logical
             end
+            if isempty(socket); socket = obj.socket; end
             result = obj.pointer.isReadable(socket);
         end
 
         function result = isWritable(obj, socket)
             arguments (Input)
                 obj (1, 1) jzmq.ZPoller
-                socket (1, 1)
+                socket = []
             end
             arguments (Output)
-                result (1, 1) boolean
+                result (1, 1) logical
             end
+            if isempty(socket); socket = obj.socket; end
             result = obj.pointer.isWritable(socket);
         end
 
         function result = isError(obj, socket)
             arguments (Input)
                 obj (1, 1) jzmq.ZPoller
-                socket (1, 1)
+                socket = []
             end
             arguments (Output)
-                result (1, 1) boolean
+                result (1, 1) logical
             end
+            if isempty(socket); socket = obj.socket; end
             result = obj.pointer.isError(socket);
         end
 
